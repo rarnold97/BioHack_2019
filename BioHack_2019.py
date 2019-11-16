16bbf0e1b70503d14b127d8ba947fe9c3060c3e5
Ryan Arnold <arnold.1016@osu.edu>
2019-11-16 10:24:17 -0500

    successfully loaded pixels from image.

--- BioHack_2019.py
+++ BioHack_2019.py
@@ -3,18 +3,24 @@
 
 # In[2]:
 
-
+#imports 
 import numpy as np
 import pandas as pd
 from matplotlib.figure import Figure 
 import matplotlib.pyplot as plt
-
+from PIL import Image
 
 # In[3]:
+#cell block for opening image file to pixel coordinates
+im = Image.open("Ryan's Eye 1.jpg")
+
+pixels = list(im.getdata())
+width, height = im.size
+
+for i, pixel in enumerate(im.getdata()):
+    print(pixel)
+
+# In[4]:
 
 
-def fun(x=0.5):
-    return x**2
-print(fun(5))
-x = np.linspace(0,100,1000)
 
