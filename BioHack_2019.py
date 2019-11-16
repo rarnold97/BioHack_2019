b2deffcd87ea5a6960a494bcb8ddd5c700b033b8
Ryan Arnold <arnold.1016@osu.edu>
2019-11-16 09:43:38 -0500

    added .py version of code

---
+++ BioHack_2019.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+from matplotlib.figure import Figure 
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+def fun(x=0.5):
+    return x**2
+print(fun(5))
+x = np.linspace(0,100,1000)
+
