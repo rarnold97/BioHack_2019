0a76f70953b76c39d583e59064c4bc9a1775aa67
bhiggins43 <higgins.400@osu.edu>
2019-11-16 15:14:48 -0500

    Vid to Img correctly

--- BioHackBenTesting.py
+++ BioHackBenTesting.py
@@ -27,25 +27,26 @@ def getFrame(sec,vidcap, cv2,count):
         cv2.imwrite("C:/Users/owner/Desktop/VidConvertTests/image"+str(count)+".jpg",image) #save frame as JPG file
     return hasFrames
 
-# In[4]:
-#eye = image2matrix()
-#eye.setGrid()
+def vidToImgs(cv2, inputVid):
+
+    vidcap = cv2.VideoCapture(inputVid)
+
+    sec = 0
+    frameRate = 30 #30 fps = on iPhone
+    count = 1
+    success = getFrame(sec,vidcap,cv2,count)
+    while success:
+        count = count + 1
+        sec = sec + 1/frameRate
+        sec = round(sec, 2)
+        success = getFrame(sec,vidcap,cv2,count)
+
+#In[4]
 import cv2
 inputVid = "TestVid.MOV"
-vidcap = cv2.VideoCapture(inputVid)
-
-# In[5]:
-sec = 0
-frameRate = 30 #it will capture image in each 0.5 second
-count = 1
-success = getFrame(sec,vidcap,cv2,count)
-while success:
-    count = count + 1
-    sec = sec + 1/frameRate
-    sec = round(sec, 2)
-    success = getFrame(sec,vidcap,cv2,count)
+vidToImgs(cv2,inputVid)
 
 print("***********")
-#plotPixels(eye)
+
 
 
--- BioHack_2019.py
+++ BioHack_2019.py
@@ -64,10 +64,32 @@ def image2matrix(imagePath = "Ryan's Eye 1.jpg"):
 def plotPixels(eyeDat):
     plt.imshow((eyeDat.df['x'],eyeDat.df['y'],eyeDat.RGB))
 
-    
+
+def vidToImgs(inputVid):
+  import cv2
+  vidcap = cv2.VideoCapture(inputVid)
+  def getFrame(sec):
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+        hasFrames,image = vidcap.read()
+        if hasFrames:
+            cv2.imwrite("image"+str(count)+".jpg",image)
+        #save frame as JPG file
+            return hasFrames
+        sec = 0
+        frameRate = 0.5 #it will capture image in each 0.5 second
+        count = 1
+        success = getFrame(sec)
+        while success:
+            count = count + 1
+            sec = sec + frameRate
+            sec = round(sec, 2)
+            success = getFrame(sec)
+
 # In[4]:
-eye = image2matrix()
-eye.setGrid()
+#eye = image2matrix()
+#eye.setGrid()
+inputVid = "TestVid.MOV"
+vidToImgs(inputVid)
 #plotPixels(eye)
 
 
